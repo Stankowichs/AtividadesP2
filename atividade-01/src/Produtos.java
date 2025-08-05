f99270aadb298b1411b953b4a3fc43d60a5daaf1
Hugo Stankowich <hstankowich@gmail.com>
2025-08-04 22:57:00 -0300

    2025-07-29

--- atividade-01/src/Produtos.java
+++ atividade-01/src/Produtos.java
@@ -4,7 +4,6 @@ public class Produtos {
     private String nome ;
     private double preco;
     private int quantidade;
-    double totalEstoque;
 
     public Produtos(String nome, double preco , int quantidade ){
         this.nome = nome;
@@ -13,7 +12,7 @@ public class Produtos {
     }
 
     public void mostrarInformaçoes(){
-        totalEstoque = quantidade * preco;
+        double totalEstoque = quantidade * preco;
         System.out.println("Informações do produto: " + nome +", R$ " + preco + ", " + quantidade + " unidades, Total: R$ " + totalEstoque);
     }
 
@@ -22,8 +21,7 @@ public class Produtos {
         System.out.println("Enter the number of products to be added in stock:");
         int novaQuantidade =  input.nextInt();
         quantidade += novaQuantidade;
-        totalEstoque = quantidade * preco;
-        System.out.println("Updated Data: " + nome +", R$ " + preco + ", " + quantidade + " unidades, Total: R$ " + totalEstoque);
+        mostrarInformaçoes();
     }
 
     public void removeProduct(){
@@ -31,7 +29,6 @@ public class Produtos {
         System.out.println("Enter the number of products to be removed in stock:");
         int novaQuantidade =  input.nextInt();
         quantidade -= novaQuantidade;
-        totalEstoque = quantidade * preco;
-        System.out.println("Updated Data: " + nome +", R$ " + preco + ", " + quantidade + " unidades, Total: R$ " + totalEstoque);
+        mostrarInformaçoes();
     }
 }
